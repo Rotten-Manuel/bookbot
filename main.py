edbb674d81a32b83ef0ad957f9c454b784534402
Rotten-Manuel <rottenimugi@gmail.com>
2025-09-14 15:22:06 -0500

    2: adding a test function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    text_file = get_book_text("./books/frankenstein.txt")
+    print(text_file)
+
+main()
